d9d5f586fc97540929dce4607fe4b9d047231735
Alexander Anpleenko <avanpleenko@gmail.com>
2022-01-30 00:44:51 +0300

    added deploy

--- src/entities/navigation/index.ts
+++ src/entities/navigation/index.ts
@@ -1,7 +1,9 @@
 import { createBrowserHistory } from 'history';
 import { createEffect } from 'effector';
 
-export const history = createBrowserHistory();
+export const history = createBrowserHistory({
+  basename: '/effector-checkout-homework',
+});
 
 export const historyPushFx = createEffect((path: string) => {
   history.push(path);
