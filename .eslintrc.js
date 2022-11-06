26abe560ef356f72d16d42b56f5aa9d75fef8bc0
Alexander Anpleenko <avanpleenko@gmail.com>
2022-11-06 18:42:25 +0300

    install eslint-kit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,37 @@
-module.exports = {
-  env: {
-    browser: true,
-    amd: true,
-    node: true,
-  },
-  globals: {
-    window: true,
-    module: true,
-  },
-  root: true,
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint', 'prettier'],
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
-};
+const { configure, presets } = require('eslint-kit');
+
+module.exports = configure({
+  mode: 'only-errors',
+  presets: [
+    presets.imports({
+      sort: {
+        newline: true,
+        groups: [
+          // Node.js builtins. You could also generate this regex if you use a `.js` config.
+          // For example: `^(${require("module").builtinModules.join("|")})(/|$)`
+          // Note that if you use the `node:` prefix for Node.js builtins,
+          // you can avoid this complexity: You can simply use "^node:".
+          [
+            '^(assert|buffer|child_process|cluster|console|constants|crypto|dgram|dns|domain|events|fs|http|https|module|net|os|path|punycode|querystring|readline|repl|stream|string_decoder|sys|timers|tls|tty|url|util|vm|zlib|freelist|v8|process|async_hooks|http2|perf_hooks)(/.*|$)',
+          ],
+          // Packages. `react` related packages come first.
+          ['^react', '^@?\\w'],
+          // Internal packages.
+          ['^(@|@company|@ui|components|utils|config|vendored-lib)(/.*|$)'],
+          // Side effect imports.
+          ['^\\u0000'],
+          // Parent imports. Put `..` last.
+          ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
+          // Other relative imports. Put same-folder imports and `.` last.
+          ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
+          // Style imports.
+          ['^.+\\.s?css$'],
+        ],
+      },
+    }),
+    presets.typescript(),
+    presets.prettier(),
+    presets.node(),
+    presets.react({ version: '18.0', newJSXTransform: true }),
+  ],
+});
