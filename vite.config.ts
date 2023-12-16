ca2b08e7dcd36ddee39fd3dac49e37a7c5a23a7c
Alexander Anpleenko <avanpleenko@gmail.com>
2023-12-16 12:45:44 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,5 @@ import { defineConfig } from 'vite';
 // eslint-disable-next-line import/no-default-export
 export default defineConfig({
   plugins: [react()],
+  base: 'effector-checkout-homework',
 });
